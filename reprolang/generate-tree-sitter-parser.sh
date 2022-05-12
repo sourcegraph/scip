a08df258f1fa25973bb7b7d3b1a4ff3f91f53477
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-05-12 11:26:42 +0200

    Fix tree-sitter CI job

--- reprolang/generate-tree-sitter-parser.sh
+++ reprolang/generate-tree-sitter-parser.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+set -eux
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
 yarn install
 ./node_modules/.bin/tree-sitter generate
