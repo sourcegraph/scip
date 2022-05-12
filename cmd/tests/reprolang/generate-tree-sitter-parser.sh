e83f2eb6d21d0dd8e58e187f8c47c5d2e8419e56
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-05-12 09:24:14 -0700

    Fix prettier nonsense.

--- cmd/tests/reprolang/generate-tree-sitter-parser.sh
+++ cmd/tests/reprolang/generate-tree-sitter-parser.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 yarn install
 ./node_modules/.bin/tree-sitter generate
+yarn --cwd ../../.. run prettier
