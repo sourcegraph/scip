1218d93eeb913e9c59235e4958260b7c30cefb93
Anton Sviridov <keynmol@gmail.com>
2023-04-11 10:51:34 +0100

    Fix tests

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -91,7 +91,7 @@ func TestSCIPSnapshots(t *testing.T) {
 		require.Nil(t, err)
 		symbolFormatter := scip.DescriptorOnlyFormatter
 		symbolFormatter.IncludePackageName = func(name string) bool { return name != testName }
-		snapshots, err := testutil.FormatSnapshots(index, "#", symbolFormatter)
+		snapshots, err := testutil.FormatSnapshots(index, "#", symbolFormatter, "")
 		require.Nil(t, err)
 		if debugSnapshotAbspaths != nil && *debugSnapshotAbspaths {
 			inputDirAbsPath, err := filepath.Abs(inputDirectory)
