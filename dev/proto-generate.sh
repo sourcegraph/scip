291283a8586bd9f30ce8a5df03917f653870e309
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-05-31 09:39:08 +0200

    Update dev/proto-generate.sh
    Co-authored-by: Varun Gandhi <varun.gandhi@sourcegraph.com>

--- dev/proto-generate.sh
+++ dev/proto-generate.sh
@@ -19,7 +19,7 @@ fi
 
 echo "--- Haskell ---"
 command -v cabal > /dev/null 2>&1 || { echo >&2 "Haskell's 'cabal' command is not installed. Please install it first via https://www.haskell.org/ghcup/"; exit 1; }
-cabal install proto-lens-protoc --overwrite-policy=always
+cabal install proto-lens-protoc-0.7.1.1 --overwrite-policy=always
 ln -sfv `which proto-lens-protoc` "$PWD/.bin/protoc-gen-haskell"
 PATH="$PWD/.bin:$PATH"
 
