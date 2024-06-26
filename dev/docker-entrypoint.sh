70dd331d01ff3c1a40b3d0cb27f31c690142f4ce
Anton Sviridov <keynmol@gmail.com>
2024-06-26 11:53:53 +0100

    Can't write to .yarnrc either...

--- dev/docker-entrypoint.sh
+++ dev/docker-entrypoint.sh
@@ -6,7 +6,10 @@ set -xeuo pipefail
 # of issues when mounting node_modules inside the docker container AND running
 # it under non-root user.
 
-echo "--modules-folder $HOME/my-node-modules" > .yarnrc
+# Symlinking won't work: https://github.com/yarnpkg/yarn/issues/8079#issuecomment-622817604
+# node_modules mounting is a known issue: https://stackoverflow.com/questions/29181032/add-a-volume-to-docker-but-exclude-a-sub-folder
+
+echo "--modules-folder $HOME/my-node-modules" > $HOME/.yarnrc
 
 export PATH="$PATH:$HOME/my-node-modules/.bin"
 export NODE_PATH="$HOME/my-node-modules"
