66c03fa824a938369a8c96067bd09049fd07fc8d
Anton Sviridov <keynmol@gmail.com>
2024-06-26 14:15:00 +0100

    Ensure we generate TS code for ABI v13

--- cmd/scip/tests/reprolang/generate-tree-sitter-parser.sh
+++ cmd/scip/tests/reprolang/generate-tree-sitter-parser.sh
@@ -3,5 +3,5 @@ set -eux
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 yarn install
-./node_modules/.bin/tree-sitter generate
+./node_modules/.bin/tree-sitter generate --abi 13
 yarn --cwd ../../../.. run prettier
