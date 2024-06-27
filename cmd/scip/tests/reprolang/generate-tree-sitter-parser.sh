76a272efd007e10f17ed42c133e261c8629ac9d0
Anton Sviridov <velvetbaldmime@protonmail.com>
2024-06-27 07:28:56 +0200

    Dockerised binding regeneration (#257)

--- cmd/scip/tests/reprolang/generate-tree-sitter-parser.sh
+++ cmd/scip/tests/reprolang/generate-tree-sitter-parser.sh
@@ -3,5 +3,8 @@ set -eux
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 yarn install
-./node_modules/.bin/tree-sitter generate
+
+# See https://github.com/smacker/go-tree-sitter/issues/85#issuecomment-1287988200
+./node_modules/.bin/tree-sitter generate --abi 13
+
 yarn --cwd ../../../.. run prettier
