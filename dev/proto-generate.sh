6990baeaf77504d86f92f89721df2999b73ad42d
Varun Gandhi <varun.gandhi@sourcegraph.com>
2024-10-17 12:57:20 +0800

    chore: Bump rust-protobuf crates to 3.7.1 (#287)
    We also bump the Rust version to 1.74.0 as older Rust
    toolchains are unable to compile the protobuf dependency

--- dev/proto-generate.sh
+++ dev/proto-generate.sh
@@ -10,9 +10,9 @@ yarn --cwd ./bindings/typescript install --frozen-lockfile
 echo "--- cargo install rust-protobuf ---"
 PROTOC_GEN_RUST_VERSION="$(cat bindings/rust/Cargo.toml | grep 'protobuf =' | sed -E 's/.*\"=(.+)\"/\1/g')"
 if ! grep -q "$PROTOC_GEN_RUST_VERSION" "./.bin/PROTOC_GEN_RUST_VERSION" \
-   || ! test -f "./.bin/bin/protoc-gen-rust"; then
+   || ! test -f "./.bin/bin/protoc-gen-rs"; then
   rm -rf .bin
-  cargo install --root .bin protobuf-codegen --version 3.2.0
+  cargo install --root .bin protobuf-codegen --version 3.7.1
   echo "$PROTOC_GEN_RUST_VERSION" > "./.bin/PROTOC_GEN_RUST_VERSION"
 fi
 
